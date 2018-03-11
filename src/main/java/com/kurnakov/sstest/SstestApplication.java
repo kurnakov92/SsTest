71cdc38ce59cf12f60378c1102e652ace338ec3a
Oleg <olegkurnakov@mail.ru>
2018-03-12 01:41:45 +0700

    Initial commit

---
+++ src/main/java/com/kurnakov/sstest/SstestApplication.java
@@ -0,0 +1,12 @@
+package com.kurnakov.sstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SstestApplication.class, args);
+	}
+}
